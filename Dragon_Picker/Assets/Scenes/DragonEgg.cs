9d5d7d7540ce11d498634a34071cb75c876f7938
Mikki158 <128837485+Mikki158@users.noreply.github.com>
2023-10-06 09:16:23 +0500

    Dragon_Picker_04

--- Dragon_Picker/Assets/Scenes/DragonEgg.cs
+++ Dragon_Picker/Assets/Scenes/DragonEgg.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DragonEgg : MonoBehaviour
 {
+    public static float bottomY = -30f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class DragonEgg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+            Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = true;
+
+        Renderer rend;
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
     }
 }
