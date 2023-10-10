2efaeece24086ed6a49281cbcfb7a81ee33d15d3
Mikki158 <128837485+Mikki158@users.noreply.github.com>
2023-10-10 09:53:56 +0500

    Dragon_Picker_13

---
+++ Dragon_Picker/Assets/_Scripts/Pause.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    private bool pause = false;
+    public GameObject panel;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!pause)
+            {
+                Time.timeScale = 0;
+                pause = true;
+                panel.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pause = false;
+                panel.SetActive(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
