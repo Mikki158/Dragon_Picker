3032f46ae7bbc1f6d3ae1c441674492b5493fd2d
Mikki158 <128837485+Mikki158@users.noreply.github.com>
2023-10-11 23:37:31 +0500

    Dragon_Picker_14

--- Dragon_Picker/Assets/_Scripts/DragonPicker.cs
+++ Dragon_Picker/Assets/_Scripts/DragonPicker.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using YG;
+using TMPro;
 
 public class DragonPicker : MonoBehaviour
 {
+    private void OnEnable() => YandexGame.GetDataEvent += GetLoadSave;
+    private void OnDisable() => YandexGame.GetDataEvent -= GetLoadSave;
+
     public GameObject energyShieldPrefab;
     public int numEnergyShield = 3;
     public float energyShieldBottomY = -6f;
     public float energyShieldRadius = 1.5f;
+    public TextMeshProUGUI scoreGT;
+
     public List<GameObject> shieldList;
 
     void Start()
     {
+        if (YandexGame.SDKEnabled == true)
+        {
+            GetLoadSave();
+        }
+
         shieldList = new List<GameObject>();
 
         for (int i = 1; i <= numEnergyShield; i++)
@@ -45,7 +57,21 @@ public class DragonPicker : MonoBehaviour
 
         if(shieldList.Count == 0)
         {
+            GameObject scoreGo = GameObject.Find("Score");
+            scoreGT = scoreGo.GetComponent<TextMeshProUGUI>();
+            UserSave(int.Parse(scoreGT.text));
             SceneManager.LoadScene("_0Scene");
         }
+    }
+
+    public void GetLoadSave()
+    {
+        Debug.Log(YandexGame.savesData.score);
+    }
+
+    public void UserSave(int currentScore)
+    {
+        YandexGame.savesData.score = currentScore;
+        YandexGame.SaveProgress();
     }
 }
