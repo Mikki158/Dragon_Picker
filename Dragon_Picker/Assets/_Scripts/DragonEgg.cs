ab95b0cd7cf959b569a07de3f0c152aeec7ecb1d
Mikki158 <128837485+Mikki158@users.noreply.github.com>
2023-10-11 18:25:24 +0500

    Dragon_Picker_14

--- Dragon_Picker/Assets/_Scripts/DragonEgg.cs
+++ Dragon_Picker/Assets/_Scripts/DragonEgg.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DragonEgg : MonoBehaviour
 {
     public static float bottomY = -30f;
+    public AudioSource audioSource;
 
 
     // Start is called before the first frame update
@@ -33,5 +34,8 @@ public class DragonEgg : MonoBehaviour
         Renderer rend;
         rend = GetComponent<Renderer>();
         rend.enabled = false;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
     }
 }
